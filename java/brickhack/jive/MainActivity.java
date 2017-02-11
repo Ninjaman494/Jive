6c866b9221aae9502330a91c976dc04af83854fa
Akash Eldo <akasheldo@hotmail.com>
2017-02-11 12:50:12 -0500

    Added tab's to action bar

--- java/brickhack/jive/MainActivity.java
+++ java/brickhack/jive/MainActivity.java
@@ -1,13 +1,92 @@
 package brickhack.jive;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        createTabs(viewPager);
+    }
+
+    private void createTabs(final ViewPager viewPager){
+        ActionBar actionBar = getSupportActionBar();
+        viewPager.setAdapter(new TabPagerAdapter(getSupportFragmentManager()));
+
+        // Specify that tabs should be displayed in the action bar.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create a tab listener that is called when the user changes tabs.
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // hide the given tab
+            }
+
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // probably ignore this event
+            }
+        };
+        // Add 3 tabs, specifying the tab's text and TabListener
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Attending")
+                        .setTabListener(tabListener));
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("All Events")
+                        .setTabListener(tabListener));
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Your Events")
+                        .setTabListener(tabListener));
+
     }
 }
+
+class TabPagerAdapter extends FragmentStatePagerAdapter {
+    private String tabTitles[] = new String[] { "Attending", "All Events", "Your Events" };
+
+    public TabPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return AllEventsFragment.newInstance(null,null);
+        /*if(position==0){
+            return lessonNotesFragment;
+        }
+        if(position==1) {
+            return grammarListFragment;
+        }
+        else{
+            return vocabListFragment;
+        }*/
+    }
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
+
